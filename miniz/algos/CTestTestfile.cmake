133afbec9b3074a438a411a918e053fded571872
Darshan Kumar Nandanwar <dnandanw@qti.qualcomm.com>
2025-07-31 11:59:41 +0000

    algos checki

---
+++ miniz/algos/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dnandanw/gitlab/bm_code/miniz/miniz
+# Build directory: /home/dnandanw/gitlab/bm_code/miniz/miniz
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(catch_tests "/home/dnandanw/gitlab/bm_code/miniz/miniz/bin/catch_tests")
+set_tests_properties(catch_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/dnandanw/gitlab/bm_code/miniz/miniz/CMakeLists.txt;325;add_test;/home/dnandanw/gitlab/bm_code/miniz/miniz/CMakeLists.txt;0;")
